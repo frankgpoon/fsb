d6dedd102de8750e32851241bd7babd935eb3234
Frank Poon <fpooooon@gmail.com>
2017-06-19 17:05:21 -0700

    Some parsing JSON completed

--- index.js
+++ index.js
@@ -44,8 +44,8 @@ exports.flashcards = function (request, response) {
 
     function findUserSet(app) {
         // get user arg and string arg from intent
-        var set = app.getArgument(SET_ARGUMENT);
-        var user = app.getArgument(USER_ARGUMENT);
+        var setName = app.getArgument(SET_ARGUMENT);
+        var userName = app.getArgument(USER_ARGUMENT);
         /* check if user is null - default to current user if true - ADD LATER BC OAUTH IS STUPID
         if (!user) {
             user = 'user_id'; // check if it works with quizlet!
@@ -54,9 +54,11 @@ exports.flashcards = function (request, response) {
        // parameters for get request
         var requestOptions = {
             host: 'api.quizlet.com',
-            path: '/2.0/users/' + user + '/sets',
+            path: '/2.0/users/' + userName + '/sets',
             headers: {'Authorization': 'Bearer ' + app.getUser.accessToken}
         };
+
+        // TODO: Handle 404 errors with user
         // callback - aka what to do with the response
         requestCallback = function (response) {
             var rawData = ''; // JSON
@@ -65,27 +67,50 @@ exports.flashcards = function (request, response) {
             });
             // once response data stops coming the request ends and we parse the JSON
             response.on('end', function () {
-                var userAllSets = JSON.parse(rawData); // all sets by user here into a JS object
 
+                var userSets = JSON.parse(rawData); // all sets by user here into a JS object
+                // processing through objects
+                var set;
+                for (i in userSets) {
+                    if (s.title === setName) {
+                        set = s;
+                    }
+                }
+                // TODO: handle set not found
 
-
+                // find undesired properties and add them to array
+                var propsToDelete = [];
+                for (var i in set) {
+                    if (i !== 'title' && i !== 'created_by' &&
+                    i !== 'term_count' && i !== 'terms') {
+                        propsToDelete.push(i);
+                    }
+                }
+                //delete undesired properties
+                for (var i in propsToDelete) {
+                    delete set[i];
+                }
+                // find undesired properties again, this time in terms
+                propsToDelete = [];
+                for (var i = 0; i < set.terms.length; i++) {
+                    for (var j in set.terms[i]) {
+                        if (j !== 'term' && j !== 'definition') {
+                            propsToDelete.push(j);
+                        }
+                    }
+                }
+                for (var i = 0; i < set.terms.length; i++) {
+                    for (var j in propsToDelete) {
+                        delete set.terms[i][j];
+                    }
+                }
+                
                 app.data.currentSet = parsedSet;
             })
         }
         // get data and end
         http.get(requestOptions, requestCallback);
     }
-
-
-    // Revive Function to filter JSON parsing
-    JSON.parse(text, (key, value) =>
-        if (key == 'terms' && value == 'term') {
-            return value;
-        } else if (key == 'terms' && value == 'definition') {
-            return value;
-        } else if (key == '') {
-            
-        }
 
 
 
